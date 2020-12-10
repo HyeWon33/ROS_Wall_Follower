c4e97fea358c8979ddeb93ea097c45ed386663ef
HyeWon33 <hjw141@naver.com>
2020-12-10 19:16:22 +0900

    Wall Follower first version

--- Ros_Wall_Follower/script/WF.py
+++ Ros_Wall_Follower/script/WF.py
@@ -1,58 +1,75 @@
-#!/home/won/.pyenv/versions/rospy3/bin/python
+#!/usr/bin/env python
+
 import math
 import rospy
 from geometry_msgs.msg import Twist
+
 from sensor_msgs.msg import LaserScan
+
 
 class SelfDrive:
     def __init__(self, publisher):
         self.publisher = publisher
-
+    
     def lds_callback(self, scan):
         turtle_vel = Twist()
         Scan_Value = average(scan.ranges[225:315])
         Scan_Value_Top = average(scan.ranges[270:315])
         Scan_Value_Bottom = average(scan.ranges[225:270])
+        Scan_Value_Middle = average(scan.ranges[265:275])
         print(Scan_Value)
+  
+        if Scan_Value >= 0.20 and Scan_Value <= 0.25:
+            turtle_vel.linear.x = 0.14
+            print("go")
+            check_SVM(Scan_Value_Middle)
 
-        def stop(self, scan):
-            if scan.ranges[0] < 0.3 and scan.ranges[0] > 0.1:
-                print(scan.ranges[0], "stop")
-                turtle_vel.linear.x = 0
-                turtle_vel.angular.z = math.pi / 2
+        if (scan.ranges[10:-10] == 0.12):
+            turtle_vel.linear.x = 0
+            turtle_vel.angular.z = (math.pi / 5)
 
-        if Scan_Value >= 0.195 and Scan_Value <= 0.22:
-            turtle_vel.linear.x = 0.18
-            stop(self,scan)
-            self.publisher.publish(turtle_vel)
+        elif Scan_Value > 0.25:
+            turtle_vel.angular.z = -(math.pi / 8)
+            turtle_vel.linear.x = 0.14 
+            print("right go")
+            check_SVM(Scan_Value_Middle)
 
-        elif Scan_Value > 0.22:
-            if (Scan_Value_Top >= Scan_Value_Bottom):
-                turtle_vel.angular.z = -(math.pi / 3)
-                turtle_vel.linear.x = 0.14
-                stop(self, scan)
-                self.publisher.publish(turtle_vel)
+            if(Scan_Value_Top > Scan_Value_Bottom):
+                turtle_vel.angular.z = -(math.pi / 8)
+                turtle_vel.linear.x = 0.14 
+                print("right go")    
+                check_SVM(Scan_Value_Middle)
             else:
-                turtle_vel.angular.z = (math.pi / 9)
-                self.publisher.publish(turtle_vel)
-                turtle_vel.linear.x = 0.18
-                stop(self, scan)
-                self.publisher.publish(turtle_vel)
-        else:
-            if (Scan_Value_Top >= Scan_Value_Bottom):
-                turtle_vel.angular.z = -(math.pi / 9)
-                turtle_vel.linear.x = 0.18
-                stop(self, scan)
-                self.publisher.publish(turtle_vel)
-            else:
-                turtle_vel.angular.z = (math.pi / 2)
+                turtle_vel.angular.z = math.pi / 4 
                 turtle_vel.linear.x = 0.14
-                stop(self, scan)
-                self.publisher.publish(turtle_vel)
+                print("left go")
+                check_SVM(Scan_Value_Middle)
+
+        elif Scan_Value < 0.2:
+            turtle_vel.angular.z = math.pi / 4 
+            turtle_vel.linear.x = 0.14
+            print("left go")
+            check_SVM(Scan_Value_Middle)
+
+            if(Scan_Value_Top > Scan_Value_Bottom):
+                turtle_vel.angular.z = -(math.pi / 8)
+                turtle_vel.linear.x = 0.14 
+                print("right go")
+                check_SVM(Scan_Value_Middle)
+            else:
+                turtle_vel.angular.z = -(math.pi / 8)
+                turtle_vel.linear.x = 0.14 
+                print("right go")
+                check_SVM(Scan_Value_Middle)
+
+        self.publisher.publish(turtle_vel)
 
 def average(list):
     return (sum(list) / len(list))
 
+def check_SVM(scaned_value):
+    if(scaned_value > 3):
+        turtle_vel.angular.z = -(math.pi / 4)
 
 def main():
     rospy.init_node('self_drive')
@@ -61,7 +78,6 @@ def main():
     subscriber = rospy.Subscriber('scan', LaserScan,
                                   lambda scan: driver.lds_callback(scan))
     rospy.spin()
-
 
 if __name__ == "__main__":
     main()
