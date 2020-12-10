35a283eb91cebe4a0c65bb2f1ba8fcfe13b2643e
HyeWon33 <hjw141@naver.com>
2020-12-10 19:53:32 +0900

    Wall_Follower perfect version

--- Ros_Wall_Follower/script/WF.py
+++ Ros_Wall_Follower/script/WF.py
@@ -1,75 +1,66 @@
-#!/usr/bin/env python
-
+#!/home/won/.pyenv/versions/rospy3/bin/python
 import math
 import rospy
 from geometry_msgs.msg import Twist
-
 from sensor_msgs.msg import LaserScan
-
 
 class SelfDrive:
     def __init__(self, publisher):
         self.publisher = publisher
-    
+
     def lds_callback(self, scan):
         turtle_vel = Twist()
-        Scan_Value = average(scan.ranges[225:315])
-        Scan_Value_Top = average(scan.ranges[270:315])
-        Scan_Value_Bottom = average(scan.ranges[225:270])
-        Scan_Value_Middle = average(scan.ranges[265:275])
-        print(Scan_Value)
-  
-        if Scan_Value >= 0.20 and Scan_Value <= 0.25:
-            turtle_vel.linear.x = 0.14
-            print("go")
-            check_SVM(Scan_Value_Middle)
+        scan_Value = average(scan.ranges[225:315])
+        scan_Value_Top = average(scan.ranges[270:315])
+        scan_Value_Bottom = average(scan.ranges[225:270])
 
-        if (scan.ranges[10:-10] == 0.12):
+        if scan_Value >= 0.195 and scan_Value <= 0.22:
+            turtle_vel.linear.x = 0.18
+            self.stop(self,scan.ranges[0])
+            self.publisher.publish(turtle_vel)
+        elif scan_Value > 0.22:
+            self.right_Top_Down_Comparison(scan, scan_Value_Top, scan_Value_Bottom, scan.ranges[0])
+        else:
+            self.left_Top_Down_Comparison(scan, scan_Value_Top, scan_Value_Bottom, scan.ranges[0])
+
+    def stop(self, scan):
+        turtle_vel = Twist()
+        if scan.ranges[0] < 0.3 and scan.ranges[0] > 0.1:
+            print(scan.ranges[0], "stop")
             turtle_vel.linear.x = 0
-            turtle_vel.angular.z = (math.pi / 5)
+            turtle_vel.angular.z = math.pi / 2
+            self.publisher.publish(turtle_vel)
 
-        elif Scan_Value > 0.25:
-            turtle_vel.angular.z = -(math.pi / 8)
-            turtle_vel.linear.x = 0.14 
-            print("right go")
-            check_SVM(Scan_Value_Middle)
-
-            if(Scan_Value_Top > Scan_Value_Bottom):
-                turtle_vel.angular.z = -(math.pi / 8)
-                turtle_vel.linear.x = 0.14 
-                print("right go")    
-                check_SVM(Scan_Value_Middle)
-            else:
-                turtle_vel.angular.z = math.pi / 4 
-                turtle_vel.linear.x = 0.14
-                print("left go")
-                check_SVM(Scan_Value_Middle)
-
-        elif Scan_Value < 0.2:
-            turtle_vel.angular.z = math.pi / 4 
+    def right_Top_Down_Comparison(self, scan_Value_Top, scan_Value_Bottom, scan_Value_Front):
+        turtle_vel = Twist()
+        if (scan_Value_Top >= scan_Value_Bottom):
+            turtle_vel.angular.z = -(math.pi / 3)
             turtle_vel.linear.x = 0.14
-            print("left go")
-            check_SVM(Scan_Value_Middle)
+            self.stop(self, scan_Value_Front)
+            self.publisher.publish(turtle_vel)
+        else:
+            turtle_vel.angular.z = (math.pi / 9)
+            self.publisher.publish(turtle_vel)
+            turtle_vel.linear.x = 0.18
+            self.stop(self, scan_Value_Front)
+            self.publisher.publish(turtle_vel)
 
-            if(Scan_Value_Top > Scan_Value_Bottom):
-                turtle_vel.angular.z = -(math.pi / 8)
-                turtle_vel.linear.x = 0.14 
-                print("right go")
-                check_SVM(Scan_Value_Middle)
-            else:
-                turtle_vel.angular.z = -(math.pi / 8)
-                turtle_vel.linear.x = 0.14 
-                print("right go")
-                check_SVM(Scan_Value_Middle)
-
-        self.publisher.publish(turtle_vel)
+    def left_Top_Down_Comparison(self, scan_Value_Top, scan_Value_Bottom, scan_Value_Front):
+        turtle_vel = Twist()
+        if (scan_Value_Top >= scan_Value_Bottom):
+            turtle_vel.angular.z = -(math.pi / 9)
+            turtle_vel.linear.x = 0.18
+            self.stop(self, scan_Value_Front)
+            self.publisher.publish(turtle_vel)
+        else:
+            turtle_vel.angular.z = (math.pi / 3)
+            turtle_vel.linear.x = 0.14
+            self.stop(self, scan_Value_Front)
+            self.publisher.publish(turtle_vel)
 
 def average(list):
     return (sum(list) / len(list))
 
-def check_SVM(scaned_value):
-    if(scaned_value > 3):
-        turtle_vel.angular.z = -(math.pi / 4)
 
 def main():
     rospy.init_node('self_drive')
@@ -79,6 +70,6 @@ def main():
                                   lambda scan: driver.lds_callback(scan))
     rospy.spin()
 
+
 if __name__ == "__main__":
     main()
-
